f19662426d9036b8333c608901fb7610cafca0ba
Manuel <manuelxgabriel@gmail.com>
2024-07-01 19:40:50 -0700

    check players names in the login

--- app/src/main/java/rodriguez/manuel/tictactoegame/AddPlayers.java
+++ app/src/main/java/rodriguez/manuel/tictactoegame/AddPlayers.java
@@ -1,6 +1,11 @@
 package rodriguez.manuel.tictactoegame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +18,37 @@ public class AddPlayers extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_players);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+
+        final EditText playerOne = findViewById(R.id.playerOneName);
+        final EditText playerTwo = findViewById(R.id.playerTwoName);
+        final Button startGameBtn = findViewById(R.id.startGameButton);
+
+
+        startGameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String getPlayerOneName = playerOne.getText().toString();
+                final String getPlayerTwoName = playerTwo.getText().toString();
+
+                // Check it has a valid name
+                if(getPlayerOneName.isEmpty() || getPlayerTwoName.isEmpty()){
+                    Toast.makeText(AddPlayers.this, "Please enter player name", Toast.LENGTH_LONG).show();
+                } else {
+                    Intent intent = new Intent(AddPlayers.this, MainActivity.class);
+                    intent.putExtra("playerOne", getPlayerOneName);
+                    intent.putExtra("playerTwo", getPlayerTwoName);
+                    startActivity(intent);
+                }
+            }
         });
+
     }
 }
--- app/src/main/java/rodriguez/manuel/tictactoegame/MainActivity.java
+++ app/src/main/java/rodriguez/manuel/tictactoegame/MainActivity.java
@@ -8,7 +8,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import rodriguez.manuel.tictactoegame.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
+
+
+
 }
